41c32971048e8551a6ec9de8fafa329c41266ea6
Ankeet <ankeethanny1@gmail.com>
2020-09-14 01:44:11 +0530

    links and advertisement section added

--- src/app/components/blog-link-banner/blog-link-banner.component.ts
+++ src/app/components/blog-link-banner/blog-link-banner.component.ts
@@ -1,15 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-blog-link-banner',
-  templateUrl: './blog-link-banner.component.html',
-  styleUrls: ['./blog-link-banner.component.css']
+  selector: "app-blog-link-banner",
+  templateUrl: "./blog-link-banner.component.html",
+  styleUrls: ["./blog-link-banner.component.css"],
 })
 export class BlogLinkBannerComponent implements OnInit {
+  links = [
+    { name: "aa", link: "a" },
+    { name: "bb", link: "b" },
+    { name: "cc", link: "c" },
+    { name: "dd", link: "d" },
+    { name: "ee", link: "e" },
+  ];
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
